c8bb75b94e443f8238e75fa5c22d56f068e52157
Piera <pierad@gmail.com>
2015-04-19 10:49:11 -0700

    Updated app/build.gradle for Android Studio

---
+++ app/build/generated/source/buildConfig/debug/me/guillaumin/android/osmtracker/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package me.guillaumin.android.osmtracker;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "me.guillaumin.android.osmtracker";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 35;
+  public static final String VERSION_NAME = "";
+}
